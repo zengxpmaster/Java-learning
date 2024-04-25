2c6dbc9f5ee72a8fa1ede604d187f359c7558cb8
zengxpmaster <1014578725@qq.com>
2024-04-25 20:22:25 +0800

    通过抽象类和抽象方法实现运行时多态

---
+++ src/april/day22/PlaneGraphics.java
@@ -0,0 +1,24 @@
+package april.day22;
+
+/**
+ * 抽象类
+ * 可以包含普通变量和方法，也可以包含抽象方法
+ * 不可以进行实例化
+ */
+public abstract class PlaneGraphics {
+    private String shape; //定义形状
+
+    public PlaneGraphics(String shape) {//定义有参构造方法
+        this.shape = shape;
+    }
+
+    public PlaneGraphics() {//定义无参构造方法
+        this("未知图形");
+    }
+
+    public abstract double area();//抽象方法没有方法体，没有{}
+
+    public void print() {
+        System.out.println(shape + "的面积是：" + area());
+    }
+}
---
+++ src/april/day22/Rectangle.java
@@ -0,0 +1,19 @@
+package april.day22;
+
+public class Rectangle extends PlaneGraphics {
+    protected double length;//长
+    protected double width;//宽
+
+    public Rectangle() {
+    }//定义无参构造方法
+
+    public Rectangle(double length, double width) {//定义有参构造方法
+        super("矩形");
+        this.length = length;
+        this.width = width;
+    }
+
+    public double area() {
+        return length * width;
+    }
+}
